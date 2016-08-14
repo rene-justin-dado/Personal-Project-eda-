068ca0cdd2cb8bb25d6c5ec1562165fc367c28cf
Justin <justindado@yahoo.com>
2016-08-14 15:58:31 +1200

    video in video

--- work/js/index.js
+++ work/js/index.js
@@ -13,9 +13,59 @@ window.addEventListener('DOMContentLoaded', () => {
   // startButton.onclick = start
   // callButton.onclick = call
   // hangupButton.onclick = hangup
-  let isStreaming = false
+let isStreaming = false,
+    myVideo = document.getElementById('localVideo'),
+    canvas = document.getElementById('canvas'),
+    grey = document.getElementById('grey')
+    context = canvas.getContext('2d'),
+    w = 650,
+    h = 420,
+    greyscale = false
 
-  const myVideo = document.getElementById('localVideo');
+myVideo.addEventListener('canplay', (evt) => {
+  if(!isStreaming) {
+    // videoWidth can be wonky in some browsers
+    if(myVideo.videoWidth > 0) {
+      h = myVideo.videoHeight / (myVideo.videoWidth / w)
+      canvas.setAttribute('width', w)
+      canvas.setAttribute('height', h)
+      // Reverse canvas image
+      context.translate(w, 0)
+      context.scale(-1, 1)
+      isStreaming = true
+    }
+  }
+}, false)
+
+// Sets up the second video at a lower z-index
+myVideo.addEventListener('play', function() {
+   // Every 33 milliseconds copy the video image to the canvas
+   setInterval(function() {
+      if (myVideo.paused || myVideo.ended) return
+      context.fillRect(0, 0, w, h)
+      context.drawImage(myVideo, 0, 0, w, h)
+      if (greyscale) goingGrey()
+   }, 33)
+}, false)
+
+// Toggle Grayness
+// grey.addEventListener('click', () => {greyscale = !greyscale}, false)
+//
+// const goingGrey = () => {
+//   const imageData = context.getImageData(0, 0, w, h)
+//   let data = imageData.data
+//   for (let i = 0; i < data.length; i += 4) {
+//     const bright = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2]
+//     data[i] = bright
+//     data[i + 1] = bright
+//    data[i + 2] = bright
+//  }
+//  context.putImageData(imageData, 0, 0)
+// }
+
+// Set Up Local video
+//
+myVideo = document.getElementById('localVideo')
   if (navigator.getUserMedia) {
     navigator.getUserMedia (
       {
