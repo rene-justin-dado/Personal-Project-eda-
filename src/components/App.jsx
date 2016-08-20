ffc3296202c684606511576f715069769770026d
EDA <eda@mac-mini-3.local>
2016-08-20 14:38:22 +1200

    scaffolding set up with react for front-end and video chat code in the back

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import request from 'superagent'
+
+
+// import videoChat from videoChatComponent
+// import poetryCorner from poetryCornerComponent
+//  Thesaurus
+//  Dictionary
+//  Rhymes
+// import tactComms from tactCommsComponent
+
+export default React.createClass ({
+  render () {
+    return (
+      <div className="appContainer">
+        <VideoChat />
+      </div>
+    )
+  }
+})
